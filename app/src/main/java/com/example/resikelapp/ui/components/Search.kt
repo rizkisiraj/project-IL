c481a60052585d5302302c9c5f54f06f7c8f8d9a
Aldi Sukma Putra <aldisumaputra6@gmail.com>
2024-11-02 00:47:34 +0700

    Mengubah ukuran searchbar

--- app/src/main/java/com/example/resikelapp/ui/components/Search.kt
+++ app/src/main/java/com/example/resikelapp/ui/components/Search.kt
@@ -3,6 +3,7 @@ package com.example.resikelapp.ui.components
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -16,8 +17,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.resikelapp.R
+import com.example.resikelapp.ui.screens.community.Community
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -32,13 +36,15 @@ fun Search(modifier: Modifier = Modifier) {
             Icon(
                 imageVector = Icons.Default.Search,
                 contentDescription = null,
-                tint = colorResource(R.color.white)
+                tint = colorResource(R.color.white),
+
             )
         },
         placeholder = {
             Text(
                 text = "Cari Komunitas",
-                color = colorResource(R.color.white)
+                color = colorResource(R.color.white),
+                fontSize = 12.sp
             )
         },
         shape = MaterialTheme.shapes.large,
@@ -47,8 +53,15 @@ fun Search(modifier: Modifier = Modifier) {
         ),
         modifier = modifier
             .padding(16.dp)
-            .fillMaxWidth()
-            .heightIn(min = 48.dp)
-    ) {
+            .width(194.dp)
+            .heightIn(min = 10.dp),
+        ) {
     }
+
+
+}
+@Preview(showBackground = true)
+@Composable
+fun SearchPreview() {
+    Search()
 }
