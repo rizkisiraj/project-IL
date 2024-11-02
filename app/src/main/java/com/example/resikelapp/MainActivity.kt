709e1dd17886b17e43de4a0f130587a9e9de4db7
Rizki Siraj <jomirwa@gmail.com>
2024-11-03 00:58:56 +0700

    finish home screen

--- app/src/main/java/com/example/resikelapp/MainActivity.kt
+++ app/src/main/java/com/example/resikelapp/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.resikelapp.ui.screen.BerandaScreen
 import com.example.resikelapp.ui.theme.ResikelAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +19,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ResikelAppTheme {
-                BerandaScreen()
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
             }
         }
     }
