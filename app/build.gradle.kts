9aaa50c4c0a1814c57ca82b18d2e3e7913991cd9
rizkisiraj <jomirwa@gmail.com>
2024-11-03 21:06:41 +0700

    fix: build gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,15 +45,12 @@ android {
         }
     }
 }
-
-dependencies {
-
     dependencies {
-        implementation(platform("androidx.compose:compose-bom:2024.01.00"))
-        implementation("androidx.compose.material:material-icons-extended")
-        implementation("androidx.compose.material3:material3")
-        implementation("androidx.compose.ui:ui-text")
-        implementation("androidx.compose.ui:ui-text-google-fonts")
+        implementation(platform(libs.androidx.compose.bom.v20240100))
+        implementation(libs.androidx.material.icons.extended)
+        implementation(libs.material3)
+        implementation(libs.androidx.ui.text)
+        implementation(libs.androidx.ui.text.google.fonts)
         implementation(libs.androidx.core.ktx)
         implementation(libs.androidx.lifecycle.runtime.ktx)
         implementation(libs.androidx.activity.compose)
@@ -63,11 +60,7 @@ dependencies {
         testImplementation(libs.junit)
         androidTestImplementation(libs.androidx.junit)
         androidTestImplementation(libs.androidx.espresso.core)
-        androidTestImplementation(platform("androidx.compose:compose-bom:2024.01.00"))
-        androidTestImplementation(libs.androidx.ui.test.junit4)
-        debugImplementation("androidx.compose.ui:ui-tooling")
+        androidTestImplementation(libs.ui.test.junit4)
+        debugImplementation(libs.ui.tooling)
         debugImplementation(libs.androidx.ui.test.manifest)
     }
-
-
-}
